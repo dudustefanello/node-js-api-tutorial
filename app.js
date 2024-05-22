a18eb6fddb2b5010dd40a98044ec7797b5000bc4
Eduardo Stefanello <eduardo@stefanello.cc>
2024-05-22 13:18:15 +0000

    lesson 13
    npm i express

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 // const {sum, add} = require('./helpers')
-
 // const total = sum(10, add(20, 30))
-
 // console.log('Total: ', total)
 
+// const http = require('http')
+// const server = http.createServer((req, res) => res.end('hello world from node.js updated'))
+// server.listen(3000)
 
-const http = require('http')
-
-const server = http.createServer((req, res) => res.end('hello world from node.js updated'))
-
-server.listen(3000)
+const express = require('express')
+const app = express()
+app.get('/', (req, res) => res.send('hello world from express'))
+app.listen(3000)
