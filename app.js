ee62316fe351b8e0f4d082e758392865217eebbb
Eduardo Stefanello <eduardo@stefanello.cc>
2024-05-22 12:55:31 +0000

    lesson 11

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-const {sum, add} = require('./helpers')
+// const {sum, add} = require('./helpers')
 
-const total = sum(10, add(20, 30))
+// const total = sum(10, add(20, 30))
 
-console.log('Total: ', total)
+// console.log('Total: ', total)
+
+
+const http = require('http')
+
+const server = http.createServer((req, res) => res.end('hello world from node.js'))
+
+server.listen(3000)
