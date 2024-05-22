32b05437ac5a2273a99886488e6be703637493c5
Eduardo Stefanello <eduardo@stefanello.cc>
2024-05-22 13:27:18 +0000

    lesson 15

--- app.js
+++ app.js
@@ -6,7 +6,11 @@
 // const server = http.createServer((req, res) => res.end('hello world from node.js updated'))
 // server.listen(3000)
 
-const express = require('express')
-const app = express()
-app.get('/', (req, res) => res.send('hello world from express'))
-app.listen(3000)
+// const express = require('express')
+// const app = express()
+// app.get('/', (req, res) => res.send('hello world from express'))
+// app.listen(3000)
+
+const fs = require('fs')
+const fileName = 'target.txt'
+fs.watch(fileName, () => console.log('file changed'))
