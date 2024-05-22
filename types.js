032d1b32c8aa6bfdb7fb2052198acab10ea63ec9
Eduardo Stefanello <eduardo@stefanello.cc>
2024-05-22 17:46:28 +0000

    lesso 7

--- types.js
+++ types.js
@@ -1,1 +1,16 @@
-var n = 1;
+var isWinter = false;
+var count = 5;
+var aName = 'Eduardo';
+var names = ['Eduardo', 26];
+var Starks;
+(function (Starks) {
+    Starks[Starks["Eduardo"] = 0] = "Eduardo";
+    Starks[Starks["Jhon"] = 1] = "Jhon";
+})(Starks || (Starks = {}));
+var cat = Starks.Eduardo;
+function getName() {
+    return 'Eduardo';
+}
+function consoleLog(message) {
+    console.log(message);
+}
--- types.ts
+++ types.ts
@@ -1,3 +1,19 @@
-var n: Number = 1
+var isWinter: boolean = false
 
-// n = 'Edu' // Typescript error
+var count: Number = 5
+
+var aName: String = 'Eduardo'
+
+var names: any[] = ['Eduardo', 26]
+
+enum Starks {Eduardo, Jhon}
+
+var cat: Starks = Starks.Eduardo
+
+function getName(): string {
+    return 'Eduardo'
+}
+
+function consoleLog(message: any): void {
+    console.log(message)
+}
