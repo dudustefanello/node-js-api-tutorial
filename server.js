1f0a5ac1e1b10612c8b8e6c3d2b150f9430575cc
Eduardo Stefanello <eduardo@stefanello.cc>
2024-05-22 14:12:18 +0000

    lesson 22 (b)

--- server.js
+++ server.js
@@ -6,7 +6,8 @@
 'use strict';
 
 const express = require('express');
-const postRoutes = require('./routes/post')
+const { getPosts } = require('./routes/post');
+const morgan = require('morgan');
 
 // Constants
 const PORT = 3000;
@@ -14,7 +15,10 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
-app.get('/', postRoutes.getPosts)
+
+app.use(morgan('dev'))
+
+app.get('/', getPosts)
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
