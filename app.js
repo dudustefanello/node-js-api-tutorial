2084828411bc0a7e403e988501732b45b98c3c39
Eduardo Stefanello <eduardo@stefanello.cc>
2024-05-22 13:48:46 +0000

    secction 1

--- app.js
+++ app.js
@@ -11,17 +11,17 @@
 // app.get('/', (req, res) => res.send('hello world from express'))
 // app.listen(3000)
 
-const fs = require('fs')
-const fileName = 'target.txt'
+// const fs = require('fs')
+// const fileName = 'target.txt'
 
-const errHandler = err => console.error(err)
-const dataHandler = data => console.log(data.toString())
+// const errHandler = err => console.error(err)
+// const dataHandler = data => console.log(data.toString())
 
-fs.readFile(fileName, (err, data) => {
-    if (err) errHandler(err)
-    dataHandler(data)
-})
-console.log('async, non blocking. This code runs first')
+// fs.readFile(fileName, (err, data) => {
+//     if (err) errHandler(err)
+//     dataHandler(data)
+// })
+// console.log('async, non blocking. This code runs first')
 
 // const data = fs.readFileSync(fileName)
 // console.log(data.toString())
